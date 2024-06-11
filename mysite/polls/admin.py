70efbfd3f6878cc10b6cc95295ca7f699d2ca0ab
bdstn10 <budistn108@gmail.com>
2024-06-11 20:40:08 +0700

    Belajar mengcustom halaman admin Django

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date Information", {"fields": ["pub_date"], "classes": ["collapse"]})
+    ]
+    
+    inlines = [ChoiceInline]
+    
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
